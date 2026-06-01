defd925774a64b1238ce057adb2436feedc1c613
umCodes <umohammed.official@gmail.com>
2026-06-01 18:46:12 +0300

    modefied backend/app/utils/json.py

--- backend/app/utils/json.py
+++ backend/app/utils/json.py
@@ -2,5 +2,4 @@ import json
 
 def parse_json_response(response: str):
     cleaned = response.replace("```json", "").replace("```JSON", "").replace("```", "").strip()
-    # print("Cleaned JSON:", json.dumps(cleaned, indent=2))  # Debugging line
     return json.loads(cleaned)
